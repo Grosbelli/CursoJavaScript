6f2598395c6710d8797c82640c8650632dc62dec
Grosbelli <agrosbelli@yahoo.com.br>
2019-01-08 18:05:30 -0200

    Att

--- exercicios-js/fundamentos/numerosAlgunsCuidados.js
+++
@@ -1,8 +0,0 @@
-console.log(7 / 0)
-console.log("10" / 2)    //em fava script da certo.
-console.log("Show" * 2)   //NaN = Not a Number
-console.log(0.1 + 0.7)
-// console.log(10.toString())
-console.log((10.345).toFixed(2))   // isso dá certo, sem precisar armazenar numa variável.
-console.log('3' + 2) //Ele vai concatenar, porque aqui a string tem a preferência. Qualquer outra sinal ele faz a conta.
-console.log('3' - 2)
